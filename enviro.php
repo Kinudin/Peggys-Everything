f0da8dbac28bbd0c8c73387e8a6b572f63e90088
Eric Cross <ericcross@Erics-iMac.local>
2013-06-05 23:25:18 -0700

    Week 10 Update
    Adding navigation.php
    updated shop.php
    refined work on shop detail page.

---
+++ enviro.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Environmental Awareness</title>
+	<link rel="stylesheet" href="reset.css">
+	<link rel="stylesheet" href="styles.css">
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,600italic,400' rel='stylesheet' type='text/css'>
+
+	<script src="jquery.js"></script>
+</head>
+<body>
+		<div class="tagLine">
+		<h1>Peggy's Everything</h1>
+		<h6>Your world is beautiful, carry it.</h6>
+	</div>
+
+	<?
+	include 'navigation.php';
+	?>
+	<div class="container" id="environment">
+		<div class="leftColumn">
+				
+				<h3> 100% Organic Material</h3>
+				<ul>
+					<li>Made from cotton materials</li>
+					<li>Cotton is completely washable, using absolutely zero plastic to fill up your trash can.</li>
+					<li>Convenient to have in your car for those unexpected shopping trip.</li>
+				</ul>
+
+
+</div>
+	<div class="rightColumn">
+				<img src="images/greenTree.jpg" alt="" width="300">
+
+	</div>
+	</div>
+	
+		<script src="script.js"></script>
+
+</body>
+</html>
